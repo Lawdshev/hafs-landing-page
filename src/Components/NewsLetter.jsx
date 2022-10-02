df7643dac920dde63774b57ad9e6e9eabdd7995b
Okunade sherif <o.d.alsherif@gmail.com>
2022-10-02 03:32:23 +0100

    EDIT7

--- src/Components/NewsLetter.jsx
+++ src/Components/NewsLetter.jsx
@@ -3,21 +3,18 @@ import newsLetterImage from '../images/WhatsApp Image 2022-07-21 at 6.04 7.png'
 
 function NewsLetter() {
   return (
-    <div className='bg-red-100 h-screen flex justify-between p-20' id='newsLetter'>
-        <div className='w-4/12' id='newsLetterImage'>
-           <div className='relative h-full w-full'>
-              <div className='bg-red-400 relative w-full h-full'></div>
-              <img src={newsLetterImage} className='absolute w-full h-full -top-6 -right-6' />
-              <div className='absolute -top-10 -right-8 h-16 w-36 border-t-black border-r-black border-solid border-2 border-l-0 border-b-0'></div>
+    <div className='bg-red-100 h-screen flex items-center justify-center' id='newsLetter'>
+           <div className=' h-3/4 w-2/4 relative dvd'>
+              <div className='bg-red-400  w-3/5 h-full wrapper'>
+                <img src={newsLetterImage} className=' w-full h-full imgg' />
+              </div>
            </div>
-        </div>
-        <div className='w-1/2 flex flex-col self-center items-center justify-around h-44 mr-12'id='newsLetterForm'>
-          <h1 className='text-4xl'id='newsLetterHeading'>Our News Letter</h1>
-          <form className='flex flex-col items-center justify-between h-1/2'>
-            <input type="email" placeholder='enter your email' className='w-96 h-8 p-4 bg-blue-50' id='newsLetterInput'/>
-            <button className='bg-black text-stone-50 h-9 w-40'id='newsLetterButton'>Subscribe now</button>
+
+          <form className='flex flex-col items-center justify-between h-52 formy'>
+            <h1 className='text-4xl' id='newsLetterHeading'>Our News Letter </h1>
+            <input type="email" placeholder='enter your email' className='mt-4 px-14 py-3 bg-blue-50' id='newsLetterInput'/>
+            <button className='bg-black text-stone-50 px-5 py-3'id='newsLetterButton'>Subscribe now</button>
           </form>
-        </div>
     </div>
   )
 }
